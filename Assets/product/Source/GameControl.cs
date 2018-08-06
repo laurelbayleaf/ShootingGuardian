91d57c56119cc5974906b007bc6d66722e6c3d5a
bay leaf <missileflier>
2018-08-07 07:51:56 +0900

    3Dシューティングゲームのチュートリアル終了
    https://hiromart.com/blog/publish_3dshootinggame/

---
+++ Assets/product/Source/GameControl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControl : MonoBehaviour
+{
+
+    public GameObject GameStartBtn;
+    public GameObject starFighterPrefab;
+    public GameObject GameOverSet;
+    public bool gameFlag;
+
+    // Use this for initialization
+    void Start()
+    {
+        gameFlag = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameFlag == false)
+        {
+            GameOverSet.SetActive(true);
+        }
+    }
+
+    public void GameStartButton()
+    {
+        Instantiate(starFighterPrefab);
+        GameStartBtn.SetActive(false);
+
+    }
+
+    public void GameOverButton()
+    {
+        Application.LoadLevel("battle");
+    }
+}
